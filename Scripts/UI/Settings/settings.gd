6e1c0739b99e6c4b59921442d82bea81d63330be
dev karol <karol.fojcik2@gmail.com>
2025-07-31 15:09:18 +0200

    Added pause menu (settings button does not work)

--- Scripts/UI/Settings/settings.gd
+++ Scripts/UI/Settings/settings.gd
@@ -1,4 +1,15 @@
 extends Control
 
+@onready var bck_t_mnu: Button = $BckTMnu
+
+var is_from_game := false
+
+func _ready() -> void:
+	if is_from_game:
+		bck_t_mnu.text = "Back to game"
+
 func _on_bck_t_mnu_pressed() -> void:
-	get_tree().change_scene_to_file("res://ui/menu/main_menu.tscn")
+	if is_from_game:
+		get_tree().change_scene_to_file("res://Scenes/level.tscn")
+	else:
+		get_tree().change_scene_to_file("res://ui/menu/main_menu.tscn")
