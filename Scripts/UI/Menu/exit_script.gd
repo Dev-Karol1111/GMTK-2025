e73cae132e78bd77f17d7b39794a4771ff8418f8
Raylock <39481834+RaylockOblivion@users.noreply.github.com>
2025-07-31 13:11:24 +0530

    Basic Main menu and theme added

---
+++ Scripts/UI/Menu/exit_script.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func pressed_btn():
+	get_tree().quit()
---
+++ Scripts/UI/Menu/transition_scene.gd
@@ -0,0 +1,8 @@
+extends Node
+
+@export var to:PackedScene
+
+#Todo write the transition code
+func transition():
+	print("Transition starting....")
+	pass
