830553ac3eee35991ff2405943bd70c538c6f0dd
Droid7770 <166847711+Droid7770@users.noreply.github.com>
2025-07-31 10:52:10 +0300

    Play button
    Makes play button change scene to main

--- Scripts/UI/Menu/transition_scene.gd
+++ Scripts/UI/Menu/transition_scene.gd
@@ -1,8 +1,9 @@
 extends Node
 
-@export var to:PackedScene
+@export var to:PackedScene = preload("res://Scenes/Main.tscn")
 
 #Todo write the transition code
 func transition():
 	print("Transition starting....")
+	get_tree().change_scene_to_packed(to)
 	pass
