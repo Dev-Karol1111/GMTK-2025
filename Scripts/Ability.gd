5539be86056c1bcf7dc333e639fe165731246377
Droid7770 <166847711+Droid7770@users.noreply.github.com>
2025-08-01 11:41:24 +0300

    Added an ability system
    Adds an ability system

---
+++ Scripts/Ability.gd
@@ -0,0 +1,41 @@
+extends Node
+class_name Ability
+# When adding new abilities you must create an enum, a corresponding function and a corresponding input event
+# All of them must have the same name, enum should be uppercase, while function and input event names must be lowercase
+enum Abilities {
+	DASH,
+	DOUBLE_JUMP,
+	OBJECT_MANIPULATION,
+}
+
+var ability_debounce: bool = false
+var debounce = false
+@export var current_ability: Abilities = Abilities.DOUBLE_JUMP
+@export var player: Player = self.get_parent()
+signal ability_changed
+var prev_ability: Abilities = current_ability
+
+func double_jump():
+	if not ability_debounce and not player.jump_debounce:
+		print("called")
+		ability_debounce = true
+		player.velocity.y = player.jump_velocity
+		await player.touched_ground
+		ability_debounce = false
+
+func _physics_process(delta: float) -> void:
+	var ability_name: String = Abilities.find_key(current_ability).to_lower()
+	if Input.is_action_just_pressed(ability_name):
+		self.call(ability_name)
+
+func on_ability_changed(c):
+	ability_debounce = false
+
+func _process(delta: float) -> void:
+	if prev_ability != current_ability:
+		ability_changed.emit(current_ability)
+	prev_ability = current_ability
+	
+func _ready() -> void:
+	ability_changed.connect(on_ability_changed)
+	
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
-
+class_name Player
 @export var speed := 300.0
 @export var jump_velocity := -400.0
-
+@export var jump_cooldown: float = 0.1
+@export var jump_debounce: bool = false # Changing to true will disable jumping
+signal touched_ground
 var is_pause_menu_opened
 
 func _ready() -> void:
@@ -15,10 +17,15 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		touched_ground.emit()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
+		jump_debounce = true
+		await get_tree().create_timer(jump_cooldown).timeout
+		jump_debounce = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
